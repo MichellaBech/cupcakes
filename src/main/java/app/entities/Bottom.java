6930940548f737451a340cc5159e6017ed26506e
Michella Stuhr Bech <144211387+MichellaBech@users.noreply.github.com>
2024-04-08 12:01:29 +0200

    designCupcake.html shoppingcart.html

--- src/main/java/app/entities/Bottom.java
+++ src/main/java/app/entities/Bottom.java
@@ -2,19 +2,12 @@ package app.entities;
 
 public class Bottom {
 
-    private int bottom_id;
-
     private String bottom_name;
 
     private int bottom_number;
 
     private double bottom_price;
 
-    public Bottom(String bottom_name, double bottom_price) {
-        this.bottom_name = bottom_name;
-        this.bottom_number = bottom_number;
-        this.bottom_price = bottom_price;
-    }
 
     public Bottom() {}
 
@@ -22,9 +15,6 @@ public class Bottom {
         return bottom_name;
     }
 
-    public int getBottom_number() {
-        return bottom_number;
-    }
 
     public double getBottom_price() {
         return bottom_price;
